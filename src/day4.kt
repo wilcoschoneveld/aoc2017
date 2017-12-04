6607b05e04decbecf900fd44fe9d96a34920aa01
Wilco Schoneveld <schoneveld.wj@gmail.com>
2017-12-04 20:23:13 +0100

    Solution to second part of day 4

--- src/day4.kt
+++ src/day4.kt
@@ -4,7 +4,15 @@ import kotlin.test.assertEquals
 fun valid(passphrase: String): Boolean {
     val words = passphrase.split(" ")
 
+    // Is the set of words equal in length to the number of words?
     return words.toSet().size == words.size
+}
+
+fun valid2(passphrase: String): Boolean {
+    val words = passphrase.split(" ")
+
+    // Is the set of sorted words equal in length to the number of words?
+    return words.map { it.toList().sorted() }.toSet().size == words.size
 }
 
 
@@ -14,4 +22,12 @@ fun main(args: Array<String>) {
     assertEquals(true, valid("aa bb cc dd aaa"))
 
     println(File("data/day4.txt").readLines().filter { valid(it) }.size)
+
+    assertEquals(true, valid2("abcde fghij"))
+    assertEquals(false, valid2("abcde xyz ecdab"))
+    assertEquals(true, valid2("a ab abc abd abf abj"))
+    assertEquals(true, valid2("iiii oiii ooii oooi oooo"))
+    assertEquals(false, valid2("oiii ioii iioi iiio"))
+
+    println(File("data/day4.txt").readLines().filter { valid2(it) }.size)
 }
