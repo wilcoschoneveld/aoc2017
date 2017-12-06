5f45dee80a16df46321b7f014bbe7b3af5b2960a
Wilco Schoneveld <schoneveld.wj@gmail.com>
2017-12-06 20:32:11 +0100

    Solution to second part of day 6

--- src/day6.kt
+++ src/day6.kt
@@ -1,34 +1,67 @@
 import java.io.File
 import kotlin.test.assertEquals
 
-fun numCycles(config: List<Int>): Int {
+fun redistribute(config: List<Int>): List<Int> {
+    // Create a mutable list which can be modified in place
     val updated = config.toMutableList()
-    val seen = hashSetOf(updated)
+
+    // Find the maximum value in the list
+    val max = updated.withIndex().maxBy { it.value }
+
+    // Throw an assertion error if the maximum could not be found (list should never be empty)
+    max ?: throw AssertionError("Could not find max in " + updated)
+
+    // Variables to keep track of redistribution
+    var i = max.index
+    var left = updated[i]
+    updated[i] = 0
+
+    // While there is 'memory' left to redistribute
+    while (left > 0) {
+        // Move to the next block and wrap around if needed
+        i = (i + 1) % updated.size
+
+        // Increment the block and remove one from the remaining stack
+        updated[i] += 1
+        left -= 1
+    }
+
+    // Return the redistributed memory, will be cast to immutable list
+    return updated
+}
+
+fun numCycles(config: List<Int>, loopOnly: Boolean = false): Int {
+    // Create a hashMap to efficiently check for uniqueness and to keep track of cycles
+    val seen = hashMapOf(config to 0)
+    var updated = config
 
     while (true) {
-        val max = updated.withIndex().maxBy { it.value }
+        // Redistribute current memory
+        updated = redistribute(updated)
 
-        max ?: throw AssertionError("Could not find max in " + updated)
+        // Check if current distribution has already been seen before
+        val before = seen[updated]
 
-        var i = max.index
-        var left = updated[i]
-        updated[i] = 0
-
-        while (left > 0) {
-            i = (i + 1) % updated.size
-            updated[i] += 1
-            left -= 1
+        // If it was seen before
+        if (before != null) {
+            // Return total number of cycles or only the loop size
+            return when(loopOnly) {
+                true -> seen.size - before
+                else -> seen.size
+            }
         }
 
-        if (seen.contains(updated)) return seen.size
-
-        seen.add(updated)
+        // Add current distribution with the number of cycles
+        seen[updated] = seen.size
     }
 }
 
 fun main(args: Array<String>) {
     assertEquals(5, numCycles(listOf(0, 2, 7, 0)))
+    assertEquals(4, numCycles(listOf(0, 2, 7, 0), true))
 
     val config = File("data/day6.txt").readLines().first().split("\t").map { it.toInt() }
+
     println(numCycles(config))
+    println(numCycles(config, true))
 }
